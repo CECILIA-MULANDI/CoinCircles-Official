6e19a797753fe8e6a1e6083aab388e5fa2149678
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-27 02:53:08 +0300

    m

--- coincircles/src/Pages/Home.jsx
+++ coincircles/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ConnectWallet from '../Components/ConnectWallet/Connect'
-import { CreateChamas } from '../Components/CallContractFunctions/CallContract'
+import  CreateChamas  from '../Components/CallContractFunctions/CallContract'
 
 export default function Home() {
   return (
