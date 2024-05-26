792de758c0a3a9e5898a91bb4dd8139d0a8777a0
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-27 02:55:04 +0300

    edit chama form

--- coincircles/src/Pages/Home.jsx
+++ coincircles/src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ConnectWallet from '../Components/ConnectWallet/Connect'
-import  CreateChamas  from '../Components/CallContractFunctions/CallContract'
+
+import ChamaForm from '../Components/Chamas/CreateChama'
 
 export default function Home() {
   return (
     <>
     <ConnectWallet/>
-    <CreateChamas/>
+    <ChamaForm/>
     </>
     
   )
