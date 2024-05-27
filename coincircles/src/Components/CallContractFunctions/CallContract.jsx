bfa628e35bfc21215d6e768155bde420c96dbdd4
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-27 04:21:19 +0300

    more

--- coincircles/src/Components/CallContractFunctions/CallContract.jsx
+++ coincircles/src/Components/CallContractFunctions/CallContract.jsx
@@ -6,7 +6,7 @@ export const connectUser = async (setWalletAddress, setIsConnected, setContract,
     if (window.ethereum) {
         try {
             await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const provider = new ethers.BrowserProvider(window.ethereum);
             setProvider(provider);
             const contract = new ethers.Contract(ContractAddress, ContractAbi.abi, provider.getSigner());
             setContract(contract);
