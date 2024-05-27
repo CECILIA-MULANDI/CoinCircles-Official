ba6ead05f1d8dc444f3b8c2d634ef1a513b8df8a
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-27 04:39:19 +0300

    more

--- coincircles/src/Components/ConnectWallet/Connect.jsx
+++ coincircles/src/Components/ConnectWallet/Connect.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from "react-bootstrap/Button";
-import { connectUser, disconnectWallet } from '../CallContractFunctions/ContractFunctions';
+import { connectUser, disconnectWallet} from '../CallContractFunctions/CallContract';
 // import "./Connect.css";
 
 export default function ConnectWallet() {
