6dc09b7e2ae0794a1b9f52c937eefb9b68e4f747
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-28 15:20:44 +0300

    more changes

--- coincircles/src/Components/NavBar/NavBar.jsx
+++ coincircles/src/Components/NavBar/NavBar.jsx
@@ -1,34 +1,51 @@
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import ConnectWallet from '../ConnectWallet/Connect';
 function NavBar() {
-  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
-  return (
-    <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="/">
-          Home
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-         About
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" disabled>
-         Contact
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3">
-        <ConnectWallet/>
-        </Nav.Link>
-      </Nav.Item>
-      
-    </Nav>
-  );
-}
+    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+  
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src="" // Replace with your logo's path
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto" variant="pills" activeKey="1" onSelect={handleSelect}>
+              <Nav.Item>
+                <Nav.Link eventKey="1" href="/">
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='/about-us'>
+                  About
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='/contact-us'>
+                  Contact
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Nav>
+              <Nav.Item>
+                <ConnectWallet />
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+  
 
 export default NavBar;
