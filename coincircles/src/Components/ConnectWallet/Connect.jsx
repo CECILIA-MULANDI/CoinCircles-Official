54d327379994131474a2d879a4f053bc98e1d1d5
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-28 16:23:52 +0300

    new changes

--- coincircles/src/Components/ConnectWallet/Connect.jsx
+++ coincircles/src/Components/ConnectWallet/Connect.jsx
@@ -1,5 +1,4 @@
-// import { ethers } from 'ethers';
-import {  useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Button from "react-bootstrap/Button";
 import { useNavigate } from 'react-router-dom';
 import { ContractAddress } from "../Constants/Constants";
@@ -7,16 +6,14 @@ import ContractAbi from "../../artifacts/contracts/Lock.sol/CoinCircles.json";
 import { connectUser } from '../CallContractFunctions/CallContract';
 import { ethers } from 'ethers';
 import { disconnectWallet } from '../CallContractFunctions/CallContract';
+
 export default function ConnectWallet() {
-  const [walletAddress,setWalletAddress] = useState(null);
+  const [walletAddress, setWalletAddress] = useState(null);
   const [error, setError] = useState(null);
-  
-  const [provider,setProvider]=useState(null);
-  // eslint-disable-next-line no-unused-vars
-  const [contract,setContract]=useState(null);
-  
+  const [provider, setProvider] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
   const navigate = useNavigate();
+
   useEffect(() => {
     async function checkConnection() {
       if (provider && walletAddress) {
@@ -26,7 +23,6 @@ export default function ConnectWallet() {
           const userEvents = events.filter(event => event.args.wallet_address === walletAddress);
           if (userEvents.length > 0) {
             setIsConnected(true);
-            navigate('/availableChamas');
           }
         } catch (error) {
           console.error('Error checking connection:', error);
@@ -37,24 +33,23 @@ export default function ConnectWallet() {
     checkConnection();
   }, [provider, walletAddress]);
 
-
- 
+  useEffect(() => {
+    if (isConnected) {
+      navigate('/availableChamas');
+    }
+  }, [isConnected, navigate]);
 
   return (
     <>
-  {walletAddress ? (
-      <>
-        <h3>Address: {walletAddress.substring(0, 5)}</h3>
-        {isConnected ? (
-          <Button className="connect-btn" onClick={()=>disconnectWallet(setWalletAddress)}>Disconnect</Button>
-        ) : (
-          <Button className="connect-btn" onClick={() => connectUser(setWalletAddress, setIsConnected, setContract, setProvider,setError)} >Connect</Button>
-        )}
-      </>
-    ) : (
-      <Button className="connect-btn" onClick={() => connectUser(setWalletAddress, setIsConnected, setContract, setProvider,setError)}>Connect</Button>
-    )}
-    {error && <p>{error}</p>}
+      {walletAddress ? (
+        <>
+          <h3>Address: {walletAddress.substring(0, 5)}</h3>
+          <Button className="connect-btn" onClick={() => disconnectWallet(setWalletAddress)}>Disconnect</Button>
+        </>
+      ) : (
+        <Button className="connect-btn" onClick={() => connectUser(setWalletAddress, setProvider, setError)}>Connect</Button>
+      )}
+      {error && <p>{error}</p>}
     </>
   );
 }
