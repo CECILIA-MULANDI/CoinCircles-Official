d3f6bafa99f0a064aae648d6dd3f4c7bdd363886
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-28 10:30:04 +0300

    new changes to the route

--- coincircles/src/App.jsx
+++ coincircles/src/App.jsx
@@ -1,8 +1,16 @@
 import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <Home/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+
+      
+    </Routes>           
+    </BrowserRouter>
+   
   );
 }
 
