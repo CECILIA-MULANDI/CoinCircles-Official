5cf5daa0678ada7aa575ec15c57f14d30ed7d35d
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-31 00:28:26 +0300

    more changes

--- coincircles/src/Components/CallContractFunctions/CallContract.jsx
+++ coincircles/src/Components/CallContractFunctions/CallContract.jsx
@@ -89,20 +89,29 @@ export const disconnectWallet = (setWalletAddress) => {
     }
   };
   
-
-
-
-
- export const getAllChamas = async () => {
+export const getAllChamas = async () => {
     try {
-      const contract = await connectToContract();
-      const chamaDetails = await contract.getAllChamas();
-      return chamaDetails;
+        const contract = await connectToContract();
+        const chamaDetails = await contract.getChamaDetails();
+        return chamaDetails;
     } catch (error) {
-      console.error('Error getting chama details:', error);
-      throw error;
+        console.error('Error getting chama details:', error);
+        throw error;
     }
-  };
+};
+
+
+
+//  export const getAllChamas = async () => {
+//     try {
+//       const contract = await connectToContract();
+//       const chamaDetails = await contract.getAllChamas();
+//       return chamaDetails;
+//     } catch (error) {
+//       console.error('Error getting chama details:', error);
+//       throw error;
+//     }
+//   };
 
 
 
@@ -182,4 +191,3 @@ export const getChamaId = async (chamaName) => {
   }
 };
 
-  
