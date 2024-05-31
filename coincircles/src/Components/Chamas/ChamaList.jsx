d13e82f9739b3b07be3cc1dddfbe9cd63d5af0c2
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-31 04:04:25 +0300

    more changes

--- coincircles/src/Components/Chamas/ChamaList.jsx
+++ coincircles/src/Components/Chamas/ChamaList.jsx
@@ -194,10 +194,10 @@ const ChamaList = () => {
                                         )}
                                     </>
                                 )}
-                                {isMember(chama, userAddress) && !contributionStatus[chama.name] && !chama.hasContributionStarted && !isContributionStarted && (
-                                    <button style={styles.button} onClick={() => handleContributeFunds(chama.name)}>
-                                        Contribute Funds
-                                    </button>
+                                {isMember(chama, userAddress) && !contributionStatus[chama.name] && (
+                                <button style={styles.button} onClick={() => handleContributeFunds(chama.name)}>
+                                    Contribute Funds
+                                </button>
                                 )}
                                 <button style={styles.button} onClick={() => handleSelectChama(chama.name)}>Select Chama</button>
                             </div>
